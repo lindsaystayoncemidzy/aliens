a670f29231557726bb3ff59f7d265c024e9db33b
lindsaystayoncemidzy <lindsaystayoncemidzy@gmail.com>
2024-03-02 18:42:37 +0800

    updated version
    small steps....

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,7 +5,7 @@ class AlienInvasion:
 
     def __init__(self):
         pygame.init()
-
+        self.clock = pygame.time.Clock()
         self.screen =pygame.display.set_mode((800,600))
         pygame.display.set_caption("alien invasion game")
 
@@ -15,6 +15,7 @@ class AlienInvasion:
                 if event.type ==pygame.QUIT:
                     sys.exit()
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__=='__main__':
     ai = AlienInvasion()
